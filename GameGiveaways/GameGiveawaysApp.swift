8ef616e49c61628d516fb825cdc4193fe63ee14c
Ali Alaa <ali_alaa@hotmail.co.uk>
2025-02-12 07:02:18 +0200

    Init Project

---
+++ GameGiveaways/GameGiveawaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameGiveawaysApp.swift
+//  GameGiveaways
+//
+//  Created by ali alaa on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameGiveawaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
