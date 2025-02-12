455dc1384a93fc4ca036faba204f563e427f9ae4
Ali Alaa <ali_alaa@hotmail.co.uk>
2025-02-12 07:44:51 +0200

    Adding Dependencies that required

--- NetworkLayer/Package.swift
+++ NetworkLayer/Package.swift
@@ -12,11 +12,19 @@ let package = Package(
             name: "NetworkLayer",
             targets: ["NetworkLayer"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkLayer"
+            name: "NetworkLayer",
+            dependencies: [
+                .product(name: "Moya", package: "Moya"),
+                .product(name: "Alamofire", package: "Alamofire")
+            ]
         ),
         .testTarget(
             name: "NetworkLayerTests",
